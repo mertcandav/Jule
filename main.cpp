ebeb60543a09823b9db8e0cf79ba73a0d699d1ef
Mertcan D <54983926+mertcandav@users.noreply.github.com>
2024-06-17 20:43:24 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 int main() {
   HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
   wchar_t text[] = L"Merhaba Dünya!";
+  std::wcout << text << std::endl;
   WriteConsoleW(hConsole, text, _countof(text), nullptr, nullptr);
   return 0;
 }
